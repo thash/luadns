38aac5c8cbd42f8142226e1a33c14ff6aeef4206
Hash <takuya21hashimoto@gmail.com>
2012-12-24 04:00:56 +0900

    CNAME log.memerelics.com -- S3 bucket website

--- memerelics.com.lua
+++ memerelics.com.lua
@@ -6,3 +6,6 @@ navi_s = "49.212.134.15"
 a(_a, navi_s, ttl)
 cname("znc", _a, ttl)
 cname("hook", _a, ttl)
+
+-- AWS::S3 web site
+cname("log", "log.memerelics.com.s3-website-ap-northeast-1.amazonaws.com", ttl)
