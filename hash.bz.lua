b24d8b171864668069730438e5b7d695b585b2e2
Hash <takuya21hashimoto@gmail.com>
2012-12-30 15:13:36 +0900

    add hash.bz

---
+++ hash.bz.lua
@@ -0,0 +1,10 @@
+ttl = 3600
+navi_s = "49.212.134.15"
+
+-- livedoor blog
+cname("m", "mobile-blog-01.livedoor.jp", ttl)
+cname("t", "blog-01.livedoor.jp", ttl)
+cname("www", "blog-01.livedoor.jp", ttl)
+
+-- zaim
+a("zakkuri", navi_s, ttl)
